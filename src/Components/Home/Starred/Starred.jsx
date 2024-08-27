1425f566f616959438010cc217fb00557b2f1389
justinait <justiiturraspe@gmail.com>
2024-08-27 19:47:26 -0300

    carouel

--- src/Components/Home/Starred/Starred.jsx
+++ src/Components/Home/Starred/Starred.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './Starred.css'
 
 function Starred() {
@@ -9,10 +9,55 @@ function Starred() {
         { name: 'Mercedes Benz', price: '9.000', image: '/cars/mercedes (1).jpg', className: ''},
         { name: 'Maserati', price: '49.900', image: '/cars/maserati (2).jpg', className: ''}
     ]
+
+    const containerRef = useRef(null);
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    useEffect(() => {
+        const container = containerRef.current;
+
+        const handleMouseDown = (e) => {
+            isDown = true;
+            startX = e.pageX - container.offsetLeft;
+            scrollLeft = container.scrollLeft;
+        };
+
+        const handleMouseLeave = () => {
+            isDown = false;
+        };
+
+        const handleMouseUp = () => {
+            isDown = false;
+        };
+
+        const handleMouseMove = (e) => {
+            if (!isDown) return;
+            e.preventDefault();
+            const x = e.pageX - container.offsetLeft;
+            const walk = (x - startX) * 3; // Multiplica para aumentar la velocidad de desplazamiento
+            container.scrollLeft = scrollLeft - walk;
+        };
+
+        container.addEventListener('mousedown', handleMouseDown);
+        container.addEventListener('mouseleave', handleMouseLeave);
+        container.addEventListener('mouseup', handleMouseUp);
+        container.addEventListener('mousemove', handleMouseMove);
+
+        return () => {
+            container.removeEventListener('mousedown', handleMouseDown);
+            container.removeEventListener('mouseleave', handleMouseLeave);
+            container.removeEventListener('mouseup', handleMouseUp);
+            container.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, []);
+
     return (
         <div>
             <h2>Coches destacados</h2>
-            <div className='starredContainerMap'>
+            
+            <div ref={containerRef} className='starredContainerMap'>
             {
                 starred.map((e, i) => {
                     return(
