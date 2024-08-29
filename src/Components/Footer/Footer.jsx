65d0fd4e57c1006bc897069d603972bbccc4cb93
justinait <justiiturraspe@gmail.com>
2024-08-29 10:23:56 -0300

    footer links

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Footer.css'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import PhoneIcon from '@mui/icons-material/Phone';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 function Footer() {
   return (
@@ -12,7 +15,12 @@ function Footer() {
         <p>Lunes a Viernes <br /> 9.30am a 1.30pm <br />4.00pm a 8.00pm</p>
 
         <h6>Contacto</h6>
-        <p>automovilespolo@gmail.com <br /> +34 660 48 51 29</p>
+        
+        <p> 
+          <MailOutlineIcon fontSize='small'/> <a href="automovilespolo@gmail.com" target='_blank' className='contactText'>automovilespolo@gmail.com</a> <br />
+          <PhoneIcon fontSize='small'/> +34 660 48 51 29 <br /> 
+          <a  className='contactText' href='https://www.instagram.com/autos_polomallorca/' target='blank'><InstagramIcon fontSize='small'/>@autos_polomallorca</a>
+        </p>
       </div>
       
       <p className='justina'>Powered by <a target='_blank' to="https://www.imjustwebs.com/"><strong > I'mJustWebs</strong></a></p>
