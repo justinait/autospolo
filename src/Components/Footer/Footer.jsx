241fac97fd173d90f1a9c8bcb879fb61d6252d1a
justinait <justiiturraspe@gmail.com>
2024-09-03 19:56:12 -0300

    desktop footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,19 +8,24 @@ function Footer() {
   return (
     <div className='footerContainer'>
       <div className='footerInfoContainer'>
-        <h6>Ubicación</h6>
-        <p>Carrer de Joan Alcover, 13, <br />Palma, Illes Balears, España</p>
+        <div className='footerInfoItem'>
+          <h6>Ubicación</h6>
+          <p>Carrer de Joan Alcover, 13, <br />Palma, Illes Balears, España</p>
+        </div>
 
-        <h6>Horario</h6>
-        <p>Lunes a Viernes <br /> 9.30hs a 13.30hs - 16hs a 18hs</p>
+        <div className='footerInfoItem'>
+          <h6>Contacto</h6>
+          <p> 
+            <PhoneIcon fontSize='small'/> +34 660 48 51 29 <br /> 
+            <a  className='contactText' href='https://www.instagram.com/autos_polomallorca/' target='blank'><InstagramIcon fontSize='small'/>@autos_polomallorca</a> <br />
+            <MailOutlineIcon fontSize='small'/> <a href="automovilespolo@gmail.com" target='_blank' className='contactText'>automovilespolo@gmail.com</a>
+          </p>
+        </div>
 
-        <h6>Contacto</h6>
-        
-        <p> 
-          <MailOutlineIcon fontSize='small'/> <a href="automovilespolo@gmail.com" target='_blank' className='contactText'>automovilespolo@gmail.com</a> <br />
-          <PhoneIcon fontSize='small'/> +34 660 48 51 29 <br /> 
-          <a  className='contactText' href='https://www.instagram.com/autos_polomallorca/' target='blank'><InstagramIcon fontSize='small'/>@autos_polomallorca</a>
-        </p>
+        <div className='footerInfoItem'>
+          <h6>Horario</h6>
+          <p>Lunes a Viernes <br /> 9.30hs a 13.30hs - 16hs a 18hs</p>
+        </div>
       </div>
       
       <p className='justina'>Powered by <a className='contactText' target='_blank' href="https://www.imjustwebs.com/"><strong > I'mJustWebs</strong></a></p>
